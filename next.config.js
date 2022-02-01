1902540a0235bc836f89bbda6f0538cd3ae00ce7
SinanGentoo <sinangentoo@gmail.com>
2022-02-01 15:21:50 +0800

    Add subpath support

--- next.config.js
+++ next.config.js
@@ -1,3 +1,23 @@
 module.exports = {
+  assetPrefix: getBasePath(),
+  basePath: getBasePath(), 
+  webpack(webpackConfig) {
+    webpackConfig.output.publicPath = getBasePath() + webpackConfig.output.publicPath; 
+    return webpackConfig;
+  },
   reactStrictMode: true,
+}
+
+function getBasePath() {
+  var basePath = "";
+
+  if (process.env.NODE_ENV === "production" && process.env.BASE_PATH) {
+    if (process.env.BASE_PATH.startsWith("/")) {
+      basePath = process.env.BASE_PATH;
+    } else {
+      basePath = "/" + process.env.BASE_PATH;
+    }
+  }
+
+  return basePath;
 }
