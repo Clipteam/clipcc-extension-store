1eb36218fb2166feea3c6fadc3c00312f2f58968
SimonShiki <sinangentoo@gmail.com>
2022-03-13 17:47:57 +0800

    Back to usercontent

--- pages/index.jsx
+++ pages/index.jsx
@@ -21,7 +21,7 @@ class Home extends React.Component {
     async componentDidMount () {
         let fetched;
         try {
-            fetched = await axios.get('https://github.com/Clipteam/clipcc-extension-list/raw/master/list.json');
+            fetched = await axios.get('https://raw.githubusercontent.com/Clipteam/clipcc-extension-list/master/list.json');
             console.log(fetched.data);
             this.setState({
                 status: 'loaded',
