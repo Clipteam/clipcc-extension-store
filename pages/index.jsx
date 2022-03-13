fed1c801e93b0bf23ab389a4d5ac12af556b362f
SimonShiki <sinangentoo@gmail.com>
2022-03-13 09:29:37 +0800

    load extension list client-side

--- pages/index.jsx
+++ pages/index.jsx
@@ -9,6 +9,30 @@ import Box from '@mui/material/Box';
 class Home extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            status: 'loading',
+            extensions: [{
+                name: 'Loading...',
+                author: 'Please wait'
+            }]
+        };
+    }
+    
+    async componentDidMount () {
+        let fetched;
+        try {
+            fetched = await axios.get('https://cdn.blestudio.com/gh/Clipteam/clipcc-extension-list/list.json');
+            console.log(fetched.data);
+            this.setState({
+                status: 'loaded',
+                extensions: fetched.data
+            });
+        } catch (error) {
+            this.setState({
+                status: 'error'
+            });
+            console.log(error);
+        }
     }
 
     render() {
@@ -19,26 +43,11 @@ class Home extends React.Component {
                 </Head>
                 <MenuBar />
                 {
-                    this.props.extensions === null? <h1>Unable to fetch extension list. <a href="">Reload</a></h1>
-                    : this.props.extensions.map((extension) => <ExtensionCard key={null} {...extension}/>)
+                    this.state.status === 'error'? <h1>Unable to fetch extension list. <a href="">Reload</a></h1>
+                    : this.state.extensions.map((extension) => <ExtensionCard key={null} {...extension}/>)
                 }
             </Box>
         )
-    }
-}
-
-export async function getServerSideProps (ctx) {
-    let fetched = null;
-    try {
-        fetched = await axios.get('https://cdn.blestudio.com/gh/Clipteam/clipcc-extension-list/list.json');
-        console.log(fetched.data);
-    } catch (error) {
-        console.log(error);
-    }
-    return {
-        props: {
-            extensions: fetched === null ? fetched.data.data : null
-        }
     }
 }
 
